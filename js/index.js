6012323676ae8a27620d62315740ca70449feffa
jcmsuperstar <68719464+jcmsuperstar@users.noreply.github.com>
2022-03-03 21:20:48 -0600

    Delete js directory

--- js/index.js
+++
@@ -1,8 +0,0 @@
-M.AutoInit();
-
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.fixed-action-btn');
-    var instances = M.FloatingActionButton.init(elems, {
-        direction: 'top'
-    });
-});
